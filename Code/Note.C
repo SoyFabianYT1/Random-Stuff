76a27946603dc8348bb197cdb43b44db6895fe62
Fabian <70019387+SoyFabianYT1@users.noreply.github.com>
2023-03-30 21:35:59 -0500

    Update Note.C

--- Code/Note.C
+++ Code/Note.C
@@ -4,5 +4,11 @@ int main()=
 
   printf("This is just a note for remembering!"\n);
   printf("Dont Forget it!!"\n);
+  
+  char test = "Fabian"
+  int age = 16
+  
+  printf("Your name is %c "\n, test)
+  printf("and your age is %d"\n, age)
   return 0;
 }
